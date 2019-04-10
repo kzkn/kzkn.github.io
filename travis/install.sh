6f8726f5d2164f5ef1ed0b171a931984ee76f83e
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2019-04-10 22:09:47 +0900

    update hugo

--- travis/install.sh
+++ travis/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p out
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.40.3/hugo_0.40.3_Linux-64bit.tar.gz >out/hugo.tgz
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.54.0/hugo_0.54.0_Linux-64bit.tar.gz >out/hugo.tgz
 cd out && tar xvf hugo.tgz
